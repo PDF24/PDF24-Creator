a9d2487fb654ba346f96219321de9cba5e4418c9
Stefan Ziegler <33447587+sz5000@users.noreply.github.com>
2026-04-29 18:02:22 +0200

    manual fix

--- manual/manual.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$content = file_get_contents('manual.html');
-$content = preg_replace('#file:[^"]+/manual.css#', 'manual.css', $content);
-$content = str_replace('<title>manual.md</title>', '<title>PDF24 Creator Manual</title>', $content);
-echo $content;
-
-?>
